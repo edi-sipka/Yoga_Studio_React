149fe44c701ee45232e05b62323d6a8ed8867eb4
edi-sipka@gmail.com <edisipka@gmail.com>
2023-01-26 00:02:42 +0100

    Hero section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Hero from './components/Hero/Hero';
+
 function App() {
   return (
     <div className="App">
-          <span style={{color: 'white'}}>Demo</span>
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./App.css"
+import Header from '../Header/Header'
+
+ const Hero = () => {
+  return (
+    <div className='hero'>
+<div className='left-h'>
+    <Header/>
+    <div className='the-best-ad'>
+        <div></div>
+        <span>Lorem ipsum lorem ipsum lorem ipsum</span>
+    </div>
+    <div className='hero-text'>
+        <div> 
+        <span className='stroke-text'>Shape</span>
+        <span>Your</span>
+    </div>
+    <div><span>Ideal Body</span></div>
+    <div>
+    <span>Lorem ipsum lorem ipsum lorem ipsum </span>
+        
+    </div>
+
+    </div>
+</div>
+<div className='right-h'></div>
+
+    </div>
+  )
+}
+export detault Hero;
