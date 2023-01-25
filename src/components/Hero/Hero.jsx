1c02727f8a6887d99e342031673134ac759482c0
edi-sipka@gmail.com <edisipka@gmail.com>
2023-01-25 16:18:27 +0100

    Inital nav setup

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Hero.css"
+
+ const Hero = () => {
+  return (
+    <div className='hero'>
+     <div className='left-h'>Left side </div>
+     <div className='right-h'> Right side </div>
+    </div>
+
+  )
+}
+
+export default Hero;
