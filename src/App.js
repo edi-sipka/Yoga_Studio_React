3a5f92d6d100fccba916e40e8f24b11bfe150479
edi-sipka@gmail.com <edisipka@gmail.com>
2023-01-31 16:29:58 +0100

    Adding program section

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 
 import './App.css';
 import Hero from './components/Hero/Hero';
+import Programs from './components/Programs/Programs';
 
 function App() {
   return (
     <div className="App">
       <Hero />
+      <Programs />
     </div>
   );
 }
