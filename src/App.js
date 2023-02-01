b46832afabc46ff1155940237ef4e0f4ac629dc7
edi-sipka@gmail.com <edisipka@gmail.com>
2023-02-01 16:29:47 +0100

    Add testimonials

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Hero from './components/Hero/Hero';
 import Programs from './components/Programs/Programs';
 import Reasons from './components/Reasons/Reasons';
 import Plans from './components/Plans/Plans';
+import Testimonials from './components/Testimonials/Testimonials';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Programs />
       <Reasons />
       <Plans />
+      <Testimonials/>
     </div>
   );
 }
