ac318e32beb453f1410876221d41bed5a2d39ba7
edi-sipka@gmail.com <edisipka@gmail.com>
2023-02-04 16:07:35 +0100

    Final stylzing

--- src/components/Reasons/Reasons.jsx
+++ src/components/Reasons/Reasons.jsx
@@ -8,6 +8,9 @@ import img4 from "../../assets/img4.jpg";
 import sony from "../../assets/sony.png";
 import voque from "../../assets/voque.png";
 import apple from "../../assets/apple.png";
+import adidas from "../../assets/adidas.png";
+import nike from "../../assets/nike.png";
+import nb from "../../assets/nb.png";
 import tick from "../../assets/tick.png";
 
 const Reasons = () => {
@@ -20,7 +23,7 @@ const Reasons = () => {
             <img src={img4}/>
         </div>
         <div className='right-r'>
-            <span>Some reasons</span>
+            <span style={{color:"white"}}>Some reasons</span>
             <div>
             <span className='stroke-text'>why</span>
             <span> choose us?</span>
@@ -42,7 +45,7 @@ const Reasons = () => {
 Treatments With Experienced Professionals
 </span></div>
 </div>
-<span style={{
+<span className='partners-gold' style={{
   color: "var(--gray)",
   fontWeight: "normal",
 }}
@@ -54,6 +57,11 @@ Our Gold partners
   <img src={apple} />
   <img src={sony} />
   <img src={voque} />
+</div>
+<div className='partners'>
+  <img src={nike} />
+  <img src={nb} />
+  <img src={adidas} />
 </div>
 </div>
 </div>
